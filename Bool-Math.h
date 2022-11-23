45bcc5a86fc2d7515d1b641e6704cac3d7048671
MelihD09 <114853415+MelihD09@users.noreply.github.com>
2022-11-23 23:17:12 +0300

    Create Bool-Math.h

---
+++ Bool-Math.h
@@ -0,0 +1,41 @@
+
+#include <stdbool.h>
+
+int main()
+{
+
+    bool x=false;
+
+    printf("%d",x);
+
+//stadbool kütüphanesi eklenir.False olduğu zaman 0 , True olduğu zaman 1 değeri basılır.
+---------------------------------------------------------------------------------------------------------------------------------
+  
+  #include <math.h>
+
+ int main()
+{
+
+//math.h ==> ceil,floor,sqrt,pow,abs
+
+printf("%f",ceil(4.854));
+
+//ceil bir üst sayıya yuvarlar (5)
+
+printf("%f",floor(4.854));
+
+//floor bir alt sayıya yuvarlar (4)
+
+printf("%f",sqrt(64));
+
+//sqrt sayının karekökünü alır (8)
+
+printf("%f",pow(2,5));
+
+//pow üst almada kullanılır ilk girilen sayı taban 2. girilen sayı üst olur (32)
+
+printf("%d",abs(-22));
+
+//abs sayının mutlak değerini alır.float veya double kullanılamaz int olmalı (22)
+
+  
